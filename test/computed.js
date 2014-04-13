82df17f1ee24c0cbf4db74a760b901b57452cf26
Carlo Zancanaro <carlo@zancanaro.id.au>
2014-04-14 09:36:01 +1000

    Add a test for changing dependencies for a  value

--- test/computed.js
+++ test/computed.js
@@ -25,7 +25,7 @@ describe('$computed', function() {
     it('should update dependent values until deregistration', function() {
         var deregister;
         scope.$apply(function() {
-            scope.val = 10; 
+            scope.val = 10;
             deregister = scope.$computed('value', function() {
                 return scope.$val('val');
             });
@@ -42,6 +42,48 @@ describe('$computed', function() {
             scope.val = 325;
         });
         expect(scope.value).toBe(25);
+    });
+
+    it('should deregister watches which are no longer used', function() {
+        var watchCounts = {};
+        scope.$watch = (function($watch) {
+            return function(expr) {
+                watchCounts[expr] = (watchCounts[expr]|0) + 1;
+                var deregister = $watch.apply(this, arguments);
+                return function() {
+                    watchCounts[expr] = (watchCounts[expr]|0) - 1;
+                    return deregister();
+                };
+            };
+        })(scope.$watch);
+
+        var deregister;
+        scope.$apply(function() {
+            scope.cond = true;
+            scope.ifTrue = 10;
+            deregister = scope.$computed('result', function() {
+                if (scope.$val('cond')) {
+                    return scope.$val('ifTrue');
+                } else {
+                    return 'ten';
+                }
+            });
+        });
+        expect(scope.result).toBe(10);
+        expect(watchCounts.cond).toBe(1);
+        expect(watchCounts.ifTrue).toBe(1);
+
+        scope.$apply(function() {
+            scope.cond = false;
+        });
+        expect(scope.result).toBe('ten');
+        expect(watchCounts.cond).toBe(1);
+        expect(watchCounts.ifTrue).toBe(0);
+
+        deregister();
+        expect(scope.result).toBe('ten');
+        expect(watchCounts.cond).toBe(0);
+        expect(watchCounts.ifTrue).toBe(0);
     });
 
 });
